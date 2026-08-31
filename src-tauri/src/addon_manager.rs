39a5785f811fc647282cc9fb61a48c273ae0cd68
BEF1721 <brian172172@gmail.com>
2026-08-31 17:44:37 -0300

    Fix addon bundle install path

--- src-tauri/src/addon_manager.rs
+++ src-tauri/src/addon_manager.rs
@@ -104,8 +104,8 @@ impl AddonManager {
 
         let mut addon_dirs = vec![config_dir.clone()];
         if let Some(resource_dir) = resource_dir {
+            addon_dirs.push(resource_dir.join("addons").join("addons"));
             addon_dirs.push(resource_dir.join("addons"));
-            addon_dirs.push(resource_dir.join("_up_").join("addons"));
         }
         for candidate in Self::dev_addon_dirs() {
             if !addon_dirs.iter().any(|dir| dir == &candidate) {
